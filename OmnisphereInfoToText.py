810fdc676282a0950a261b52672bc2148baeedfe
Pashan Irani <pashanirani@gmail.com>
2019-01-12 11:23:59 -0800

    made it read files recursively

--- OmnisphereInfoToText.py
+++ OmnisphereInfoToText.py
@@ -7,23 +7,28 @@ import datetime, os
 """ I know this code be written better, but I had to be FAST!"""
 def run(args):
     try:
-        out = open(args.input, 'w')
+        out = open(args.input, 'w', encoding="utf8")
     except AttributeError:
-        out = open('OmniPresetsToText.txt', 'w')
+        out = open('OmniPresetsToText.txt', 'w', encoding="utf8")
 
     pattern = re.compile('<ENTRYDESCR  name=')
 
     out.write("Omnishpere presets in .flp files from " + os.path.dirname(os.path.realpath(__file__)) + " written on [" + datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y") + "]\n\n")
     out.write("~ A Program created by Pashan\n")
     out.write("https://github.com/PashanIrani/OmnispherePresetInfoToText.git\n\n")
-
-    for filename in glob.glob("*.flp"):
-        flp = open(filename, "r")
+	
+    count = 0
+    total_projects = 0
+	
+    for filename in glob.iglob("../**/*.flp", recursive=True):
+        
+        flp = open(filename, "r", encoding="ansi")
 
         list = ""
-
+		
+        count += 1
+		
         for line in flp:
-
             matchFound = pattern.match(line)
             if matchFound:
                 el = ET.fromstring(line + "</ENTRYDESCR>")
@@ -33,11 +38,13 @@ def run(args):
                     list = list + presetName
 
         if list != "":
+            total_projects += 1
             out.write(filename + ':\n' + list + "\n")
 
-        print('Done: ' + filename + "")
+        print('[' + str(count) + '] Done: ' + filename + "")
 
-
+    out.write("total_projects:" + str(total_projects))
+	
 def main():
     parser = argparse.ArgumentParser(description="Gets Omnishpere plugin names from .flps and outputs to a text file")
     parser.add_argument("-out",help="name of output file" , dest="output", type=str, required=False, default='OmniPresetsToText.txt')
---
+++ readme.md.txt
@@ -0,0 +1,5 @@
+# Instructions
+1. Navigate to this dir.
+2. Run `python OmnisphereInfoToText.py`
+3. It will search for .flp files in ALL dirs this DIR is in.
+4. Output is in this DIR.
